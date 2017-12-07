90c7e2083413ec109ac80127f222d3783f8b25e0
P-Russell <pdrussell011@gmail.com>
2017-12-07 09:03:48 +0200

    Cmd line sanitation

--- interface.py
+++ interface.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 from expert import *
 
@@ -14,11 +16,14 @@ def main():
     if len(sys.argv) == 1:
         print("Gimme file, I need file")
         exit(1)
-    with open(sys.argv[1], "r") as file:
-        facts = FactCollection.from_file(file)
-    for q in facts.queries:
-        facts.facts[q].status = facts.truth_check(q)
-        print(q,"is", facts.facts[q].status)
+    try:
+        with open(sys.argv[1], "r") as file:
+            facts = FactCollection.from_file(file)
+        for q in facts.queries:
+            facts.facts[q].status = facts.truth_check(q)
+            print(q,"is", facts.facts[q].status)
+    except OSError:
+        print("Gimme valid file, I need file")
 
 if __name__ == "__main__":
     main()
