9ff1a4c9fc1d2a07eaf7ba35d9a4503b457598d2
P-Russell <pdrussell011@gmail.com>
2017-12-14 15:59:53 +0200

    better sudo code for shunting

--- shunting.py
+++ shunting.py
@@ -1,28 +1,16 @@
-# while there are tokens to be read:
-# 	read a token.
-# 	if the token is a number, then push it to the output queue.
-# 	if the token is an operator, then:
-# 		while (there is an operator at the top of the operator stack with
-# 			greater precedence) or (the operator at the top of the operator stack has
-#                         equal precedence and
-#                         the operator is left associative) and
-#                       (the operator at the top of the stack is not a left bracket):
-# 				pop operators from the operator stack, onto the output queue.
-# 		push the read operator onto the operator stack.
-# 	if the token is a left bracket (i.e. "("), then:
-# 		push it onto the operator stack.
-# 	if the token is a right bracket (i.e. ")"), then:
-# 		while the operator at the top of the operator stack is not a left bracket:
-# 			pop operators from the operator stack onto the output queue.
-# 		pop the left bracket from the stack.
-# 		/* if the stack runs out without finding a left bracket, then there are
-# 		mismatched parentheses. */
-# if there are no more tokens to read:
-# 	while there are still operator tokens on the stack:
-# 		/* if the operator token on the top of the stack is a bracket, then
-# 		there are mismatched parentheses. */
-# 		pop the operator onto the output queue.
-# exit.
+# 1.  While there are tokens to be read:
+# 2.        Read a token
+# 3.        If it's a number add it to queue
+# 4.        If it's an operator
+# 5.               While there's an operator on the top of the stack with greater precedence:
+# 6.                       Pop operators from the stack onto the output queue
+# 7.               Push the current operator onto the stack
+# 8.        If it's a left bracket push it onto the stack
+# 9.        If it's a right bracket
+# 10.            While there's not a left bracket at the top of the stack:
+# 11.                     Pop operators from the stack onto the output queue.
+# 12.             Pop the left bracket from the stack and discard it
+# 13. While there's operators on the stack, pop them to the queue
 
 # The following symbols are defined, in order of decreasing priority:
 # • ( and ) which are fairly obvious. Example : A + (B | C) => D
